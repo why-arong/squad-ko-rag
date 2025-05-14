def5150bab301ac3e927e623ac028da2bee3ce38
why-arong <pilmokim99@gmail.com>
2025-05-14 16:30:52 +0900

    add error handling

--- app/llms/huggingface.py
+++ app/llms/huggingface.py
@@ -15,16 +15,21 @@ class HuggingFaceLLM:
             bnb_4bit_compute_dtype="float16",
             bnb_4bit_use_double_quant=True,
         )
-        chat_model = HuggingFacePipeline.from_model_id(
-            model_id='LGAI-EXAONE/EXAONE-3.5-2.4B-Instruct',
-            task='text-generation',
-            pipeline_kwargs=dict(
-                max_new_tokens=1024,
-                do_sample=False,
-                repetition_penalty=1.03
-            ),
-            model_kwargs={'quantization_config': quantization_config, "trust_remote_code": True, "device_map": "auto"}
-        )
+        try:
+            chat_model = HuggingFacePipeline.from_model_id(
+                model_id='LGAI-EXAONE/EXAONE-3.5-2.4B-Instruct',
+                task='text-generation',
+                pipeline_kwargs=dict(
+                    max_new_tokens=1024,
+                    do_sample=False,
+                    repetition_penalty=1.03
+                ),
+                model_kwargs={'quantization_config': quantization_config, "trust_remote_code": True,
+                              "device_map": "auto"}
+            )
+        except Exception as e:
+            logger.error("Failed to load LLM:", exc_info=e)
+            raise
         self.model = ChatHuggingFace(llm=chat_model, model_id='LGAI-EXAONE/EXAONE-3.5-2.4B-Instruct')
 
     def generate_answer(self, query: QueryRequest):
