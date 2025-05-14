904a9ad0ae34baa16756872dcc245b338cfcb226
pilmo kim <68311908+why-arong@users.noreply.github.com>
2025-05-14 23:57:36 +0900

    docs: Update README with Korean localization and detailed project usage (#3)
    * hotfix: add model id to chf
    
    * fix:modelname
    
    * refactor: add trust_remote_code
    
    * feat: change embedding model to hf
    
    * add device map in model kwargs
    
    * add error handling
    
    * hotfix: add logger module
    
    * fsd
    
    * feat: add re for parsing answer
    
    * feat: add helper funtion for parsing
    
    * Add readme filegit add .git add .git add .
    gp
    
    * feat: update readme

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 
+
 class QueryRequest(BaseModel):
     question: str
 
+
 class QueryResponse(BaseModel):
-    retrieved_document_id: str
-    retrieved_document: str
     question: str
     answer: str
+    retrieved_document_id: str
+    retrieved_document: str
